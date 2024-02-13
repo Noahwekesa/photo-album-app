d3defab22827bf57e008bd6a328468895429c6e0
noahwekesa <noahwekesa0@gmail.com>
2024-02-13 18:34:05 +0300

    templates

--- photos/views.py
+++ photos/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from .models import Photo
 
 
 def index(request):
+    photos = Photo.objects.all().order_by("-created_at")
     context = {}
     return render(request, "index.html", context)
+
+
+def addPhoto(request):
+    context = {}
+    return render(request, "add.html", context)
+
+
+def viewPhoto(request, pk):
+    context = {}
+    return render(request, "view_photo.html", context)
