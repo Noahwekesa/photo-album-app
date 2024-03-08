cb2253ccd1b7115df10100817f6c4d8b385edf5f
noahwekesa <noahwekesa0@gmail.com>
2024-03-08 23:50:18 +0300

    upload image

--- photos/models.py
+++ photos/models.py
@@ -28,7 +28,7 @@ class Photo(models.Model):
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True, blank=True
     )
-    image = models.ImageField(upload_image="media/", null=False, blank=False)
+    image = models.ImageField(upload_to="media/", null=False, blank=False)
     description = models.TextField()
 
     def __str__(self):
